197302804c056a682fa1083241ae394a51f3e9dd
AntonioPaes07 <juniorpaes42@gmail.com>
2025-03-24 23:48:27 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Rafaela, você é uma pessoa muito incrivel, nunca se esqueça disso! Se voce gostou dessa mensagem, poderia me dar um abraço?</h1>
+    return <h1>Rafaela, saiba que você é uma pessoa muito incrivel, nunca se esqueça disso! Obrigado por me ajudar e incentivar a ser uma pessoa melhor! Se voce gostou dessa mensagem, poderia me dar um abraço?</h1>
 }
 
 export default Home; 
