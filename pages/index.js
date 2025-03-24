ae5b789d1c413f0607e9435d40e029fecf37ff04
AntonioPaes07 <juniorpaes42@gmail.com>
2025-03-24 22:38:52 +0000

    adiciona pagina incial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Rafaela, você é uma pessoa muito incrivel, nunca se esqueça disso! Se voce gostou dessa mensagem, poderia me dar um abraço?</h1>
+}
+
+export default Home; 
